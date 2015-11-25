cf8511132249b37afa5fe128762216f1fda4be6b
Aditya Viswanathan <aditya@adityaviswanathan.com>
2015-11-24 20:43:29 -0600

    added advanced features, integration into ai_trainer with labels pending

--- ai_trainer.py
+++ ai_trainer.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 import sys
 from sklearn import preprocessing
+from pprint import pprint
 
 sys.path.append('data')
 
@@ -14,12 +15,14 @@ import featurizer as fz
 import coordKeys as keysXY
 import coordKeysZ as keysXYZ
 
-
 class Personal_Trainer:
 
 	def __init__(self, key):
 		self.key = key
 		self.squats = []
+		self.keys_to_indices = {}
+		for i, key in enumerate(keysXYZ.columns):
+			self.keys_to_indices[key] = i
 
 	def load_squats(self, file):
 		self.squats = pickle.load(open(file,"rb"))
@@ -55,10 +58,35 @@ class Personal_Trainer:
 		feature_vectors = []
 		labels = []
 		
-		#=====[ Extract features for each squat  ]=====
+		#=====[ Extract basic features for each squat  ]=====
 		for squat in squats:
 			feature_vectors.append(fz.extract_basic(squat[0], self.key))
 			labels.append(squat[1])
+
+		advanced_labels = []
+		#=====[ Extract advanced features for each squat  ]=====
+
+		# CURRENTLY STORING IN DICT, PROBABLY NEED TO RESTRUCTURE LATER
+		advanced_feature_vector = {}
+		advanced_feature_vector['stance_shoulder_width'] = []
+		advanced_feature_vector['stance_straightness'] = []
+		advanced_feature_vector['feet'] = []
+		advanced_feature_vector['bend_hips_knees'] = []
+		advanced_feature_vector['back_straight'] = []
+		advanced_feature_vector['head_aligned_back'] = []
+		advanced_feature_vector['depth'] = []
+		advanced_feature_vector['back_hip_angle'] = []
+		for squat in squats:
+			advanced_feature_vector['stance_shoulder_width'].append(fz.extract_stance_shoulder_width(squat[0], self.key, self.keys_to_indices))
+			advanced_feature_vector['stance_straightness'].append(fz.extract_stance_straightness(squat[0], self.key, self.keys_to_indices))
+			advanced_feature_vector['feet'].append(fz.extract_feet(squat[0], self.key, self.keys_to_indices))
+			advanced_feature_vector['bend_hips_knees'].append(fz.bend_hips_knees(squat[0], self.key, self.keys_to_indices))
+			advanced_feature_vector['back_straight'].append(fz.back_straight(squat[0], self.key, self.keys_to_indices))
+			advanced_feature_vector['head_aligned_back'].append(fz.head_aligned_back(squat[0], self.key, self.keys_to_indices))
+			advanced_feature_vector['depth'].append(fz.depth(squat[0], self.key, self.keys_to_indices))
+			advanced_feature_vector['back_hip_angle'].append(fz.back_hip_angle(squat[0], self.key, self.keys_to_indices))
+
+			# MISSING LABELS
 
 		#=====[ Return X, and y ]=====
 		X = np.concatenate(feature_vectors,axis=0)
